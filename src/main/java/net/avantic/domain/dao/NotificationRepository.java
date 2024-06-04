24e8838232b9f982f5540e8dd286257f6556a407
Alexis Rodríguez <arodriguez@avantic.net>
2024-06-04 13:33:04 +0100

    Refactor

--- src/main/java/net/avantic/domain/dao/NotificationRepository.java
+++ src/main/java/net/avantic/domain/dao/NotificationRepository.java
@@ -1,5 +1,6 @@
 package net.avantic.domain.dao;
 
+import net.avantic.domain.model.Empleado;
 import net.avantic.domain.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +9,6 @@ import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    @Query("From Notification n where n.notificada = false")
-    List<Notification> findAllByNotNotificada();
+    @Query("From Notification n where n.notificada = false and n.empleado = :empleado")
+    List<Notification> findAllByNotNotificadaAndEmpleado(Empleado empleado);
 }
--- src/main/java/net/avantic/job/validarjornada/NotificarViaEmailJob.java
+++ src/main/java/net/avantic/job/validarjornada/NotificarViaEmailJob.java
@@ -1,38 +1,51 @@
 package net.avantic.job.validarjornada;
 
+import jdk.jshell.spi.ExecutionControl;
+import net.avantic.domain.dao.EmpleadoRepository;
 import net.avantic.domain.dao.NotificationRepository;
+import net.avantic.domain.model.Empleado;
 import net.avantic.domain.model.Notification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class NotificarViaEmailJob {
 
     private final NotificationRepository notificationRepository;
+    private final EmpleadoRepository empleadoRepository;
 
     @Autowired
-    public NotificarViaEmailJob(NotificationRepository notificationRepository) {
+    public NotificarViaEmailJob(NotificationRepository notificationRepository,
+                                EmpleadoRepository empleadoRepository) {
         this.notificationRepository = notificationRepository;
+        this.empleadoRepository = empleadoRepository;
     }
 
-    //todo arodriguez
-    /*
-        Para no enviar un email por cada incidencia lo interesante sería:
-        1. iterar para cada empleado y hacer un notificationRepository.findAllByNotNotificadaAndEmpleado
-        2. construimos un solo emailDto con todas las incidencias y lo enviamos
-        3. Marcamos las incidencias como notificadas si el envío ha tenido éxito
-     */
     @Scheduled(fixedDelay = 5000)
     public void notificar() {
-        notificationRepository.findAllByNotNotificada().stream()
-                .forEach(this::internalNotificar);
+        empleadoRepository.findAll().stream()
+                    .forEach(emp -> {
+                        try {
+                            List<Notification> notifications = notificationRepository.findAllByNotNotificadaAndEmpleado(emp);
+                            buildAndSendEmailFromNotifications(emp, notifications);
+                        } catch (Exception e) {
+
+                        }
+                    });
     }
 
 
-    private void internalNotificar(Notification notification) {
-
-        notification.setNotificada(true);
-        notificationRepository.save(notification);
+    private void buildAndSendEmailFromNotifications(Empleado empleado, List<Notification> notifications) {
+        //todo arodriguez
+        //emailService.buildAndSend(empleado, notifications);
+        notifications.stream()
+                .forEach(n -> {
+                    n.setNotificada(true);
+                    notificationRepository.save(n);
+                });
     }
+
 }
